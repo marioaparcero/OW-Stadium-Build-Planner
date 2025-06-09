509ba278d9203fb875e1557aa71ffb82cd479674
Mario Aparcero <marioaparcero@hotmail.com>
2025-06-09 02:23:54 +0200

    Upd: Update webpack configuration: Change devServer port to 3001 and add allowed hosts for development

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,9 +8,14 @@ module.exports = merge(base, {
   devtool: 'eval',
   devServer: {
     static: path.join(__dirname, 'build'),
-    port: 3000,
+    port: 3001,
     https: true,
     historyApiFallback: true,
+    allowedHosts: [
+      'builds.comunidadoverwatch.com',
+      '.comunidadoverwatch.com',
+      'localhost',
+    ],
   },
   output: {
     assetModuleFilename: 'assets/[name][ext][query]',
